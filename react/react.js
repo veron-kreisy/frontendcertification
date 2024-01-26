bb690b9f333285ac537ff9e34fb11057e2b3fe5c
miss_codein <mbula0563@gmail.com>
2024-01-26 16:21:53 +0300

    Update react.js

--- react/react.js
+++ react/react.js
@@ -1,1 +1,33 @@
-const JSX = <div></div>;
+// react is an Open Source view library 
+// It's a great tool to render the User Interface (UI) of modern web applications.
+
+// ReactDOM.render(JSX, document.getElementById('root'))
+
+
+
+// const JSX = <h1>Hello JSX!</h1>;
+ 
+// const JSX =<div>
+//     <h1></h1>
+//     <p></p>
+//     <ul>
+//     <li></li>
+//     <li></li>
+//     <li></li>
+// </ul>
+    
+// </div>
+
+
+
+// adding comments {/* */}
+// react rendering api known as react DOM
+
+// ReactDOM.render(componentToRender, targetNode)
+// component you want to render and DOM node ypu want to render to
+ReactDOM.render (JSX,document.getElementById('challenge-node'));
+const JSX = (
+    <div className="myDiv">
+      <h1>Add a class to this div</h1>
+    </div>
+  );
