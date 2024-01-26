075ac0c657cf5eac719f2fca23eff4f98ac415fe
miss_codein <mbula0563@gmail.com>
2024-01-26 23:23:07 +0300

    Update react.js

--- react/react.js
+++ react/react.js
@@ -82,21 +82,7 @@ const MyComponent = () => {
     }
   };
 
-  const ChildComponent = () => {
-    return <p>I am the child!</p>;
-  };
   
-  class ParentComponent extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>I am the parent!</h1>
-          {/* Rendering ChildComponent within ParentComponent */}
-          <ChildComponent />
-        </div>
-      );
-    }
-  }
 
 
 
