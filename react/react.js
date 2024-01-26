2bbd1f7f62282f3456061b6d2d60d4de529425e1
miss_codein <mbula0563@gmail.com>
2024-01-26 23:14:05 +0300

    Update react.js

--- react/react.js
+++ react/react.js
@@ -39,3 +39,14 @@ const MyComponent = () => {
       </div>
     );
   };
+  class Kitten extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <h1>Hi</h1>
+      );
+    }
+  }
